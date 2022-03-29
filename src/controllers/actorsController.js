14cb3f0ca1f72d000d6ed4ab8c8b88bdf589b1c3
juanpampean <66573948+juanpampean@users.noreply.github.com>
2022-03-29 17:21:00 -0300

    ejercicio bonus

---
+++ src/controllers/actorsController.js
@@ -0,0 +1,21 @@
+const db = require('../database/models');
+const sequelize = db.sequelize;
+
+
+const actorsController = {
+    'list': (req, res) => {
+        db.Actor.findAll()
+            .then(actors => {
+                res.render('actorsList.ejs', {actors})
+            })
+    },
+    'detail': (req, res) => {
+        db.Actor.findByPk(req.params.id)
+            .then(actor => {
+                res.render('actorsDetail.ejs', {actor});
+            });
+    },
+
+  }
+
+module.exports = actorsController;
